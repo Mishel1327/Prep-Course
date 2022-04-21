ca28e79e08575d27d77587775917c20e084b361c
Mishel Garcia <mechyxd95@gmail.com>
2022-04-20 22:53:00 -0300

    Termine HTML y CSS

---
+++ 02-JS-I/mishel_estudia.js
@@ -0,0 +1,50 @@
+
+// variables ejemplos
+
+var variable = 5 // var se pueden modificar durante la ejecucion
+const constante = 1 // const no se puede modificar
+
+
+var string = "cadena" // variable de string
+var enteros = 1 // variable de enteros
+var boolean = true // variable booleana
+
+
+var nuevaResta = (10 - 5) === 5;
+
+var x = 5;
+var y = 2;
+
+// funcion ejemplo
+
+function multiplicacion(param1 , param2) {
+    return (param1 * (param1 + param2));;
+}
+console.log(multiplicacion(x,y))
+
+// length ejemplos
+
+var str = "asdasd";
+
+console.log(str.length)
+
+
+console.log("asd" !== "asd")
+
+//operadores
+
+< // izquiera es menor al de la derecha de la
+> // el de arriba pero al revez
+=== // compara 2 cosas
+/ //divison
+* //multiplicacion
+% // mod
++ // suma
+- // resta
+!= // distinto
+
+
+
+
+
+
